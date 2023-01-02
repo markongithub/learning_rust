8d0e920a7d28212ddb40d25c93a3f956fcc4e5f2
mark on github <markongithub@users.noreply.github.com>
2023-01-02 13:06:32 -0500

    still more or less working

--- avl_tree.rs
+++ avl_tree.rs
@@ -1,7 +1,7 @@
 use std::cmp::max;
 use std::fmt::Debug;
-use std::mem::replace;
-use std::mem::swap;
+// use std::mem::replace;
+// use std::mem::swap;
 
 type AVLTree<T> = Option<Box<AVLNode<T>>>;
 
@@ -22,16 +22,24 @@ fn get_height<T: Ord>(tree: &AVLTree<T>) -> i64 {
 
 fn balance_factor<T: Ord>(tree: &AVLTree<T>) -> i64 {
     match tree {
-        &Some(ref node) => get_height(&node.right) - get_height(&node.left),
+        &Some(ref node) => get_height(&node.left) - get_height(&node.right),
         &None => 0,
     }
 }
 
-fn insert<T: Ord>(tree: &mut AVLTree<T>, new_label: T) {
-    println!("here we go");
+fn rebalance<T: Ord + Debug>(tree: &mut AVLTree<T>) {
+    let bf = balance_factor(tree);
+    match bf {
+        -1 | 0 | 1 => (),
+        -2 => rotate_left_maybe_double(tree),
+        2 => rotate_right_maybe_double(tree),
+        x => panic!("Bad balance factor: {0}", x),
+    }
+}
+
+fn insert<T: Ord + Debug>(tree: &mut AVLTree<T>, new_label: T) {
     let option_amp_mut: Option<&mut Box<AVLNode<T>>> = tree.as_mut();
     if option_amp_mut.is_some() {
-        println!("option_amp_mut is Some.");
         let mut_box: &mut Box<AVLNode<T>> = option_amp_mut.unwrap();
         let this_label: &T = &mut_box.label;
         let child: &mut AVLTree<T> = if new_label < *this_label {
@@ -42,7 +50,14 @@ fn insert<T: Ord>(tree: &mut AVLTree<T>, new_label: T) {
             panic!("What if we insert the same key twice?")
         };
         insert(child, new_label);
+        let left_height = get_height(&mut_box.left);
+        let right_height = get_height(&mut_box.right);
+        println!(
+            "After inserting, left height is {0} and right height is {1}",
+            left_height, right_height
+        );
         mut_box.height = max(get_height(&mut_box.left), get_height(&mut_box.right)) + 1;
+        rebalance(tree);
     } else {
         println!("option_amp_mut is None.");
         let new_tree: AVLTree<T> = singleton(new_label);
@@ -80,55 +95,14 @@ fn contains<T: Eq + Ord + Debug>(tree: &AVLTree<T>, target: T) -> bool {
     }
 }
 
-/*
-node *rotate_Left(node *X, node *Z) {
-    // Z is by 2 higher than its sibling
-    t23 = left_child(Z); // Inner child of Z
-    right_child(X) = t23;
-    if (t23 != null)
-        parent(t23) = X;
-    left_child(Z) = X;
-    parent(X) = Z;
-    // 1st case, BF(Z) == 0,
-    //   only happens with deletion, not insertion:
-    if (BF(Z) == 0) { // t23 has been of same height as t4
-        BF(X) = +1;   // t23 now higher
-        BF(Z) = –1;   // t4 now lower than X
-    } else
-    { // 2nd case happens with insertion or deletion:
-        BF(X) = 0;
-        BF(Z) = 0;
-    }
-    return Z; // return new root of rotated subtree
-}
-
-For a right rotation:
-the new root will be the old left child
-the old root will be the new right child
-the new root's left child doesn't change at all
-the old root's right child doesn't change at all
-the new root's old right child will be the old root's new left child
-
-old root D
-new root B
-B's left child A does not change
-D's right child E does not change
-B's right child C becomes the new left child of D
-
-root pointer changes from D to B
-D.left changes from B to C
-B.right changes from C to D
-
-*/
-
 fn rotate_left<T: Ord>(old_root_x: &mut AVLTree<T>) {
     if old_root_x.as_mut().is_none() {
         return;
     }
     if old_root_x.as_mut().unwrap().right.as_mut().is_none() {
-        panic!("This is too weird");
+        panic!("I am rotatingleft but my right child is None.");
     }
-    let orphan_t23: &mut AVLTree<T> = &mut old_root_x
+    let right_then_left_child: &mut AVLTree<T> = &mut old_root_x
         .as_mut()
         .unwrap()
         .right
@@ -136,16 +110,127 @@ fn rotate_left<T: Ord>(old_root_x: &mut AVLTree<T>) {
         .unwrap()
         .left
         .take();
-    let orphan_z: &mut AVLTree<T> = &mut old_root_x.as_mut().unwrap().right.take();
-    old_root_x.as_mut().unwrap().right = orphan_t23.take();
-    orphan_z.as_mut().unwrap().left = old_root_x.take();
-    *old_root_x = orphan_z.take();
+    let old_right_child: &mut AVLTree<T> = &mut old_root_x.as_mut().unwrap().right.take();
+    let right_height = get_height(right_then_left_child);
+    let left_height = get_height(&old_root_x.as_mut().unwrap().left);
+    println!(
+        "New left height is {0} and right height is {1}",
+        left_height, right_height
+    );
+    let new_height: i64 = 1 + max(left_height, right_height);
+    old_root_x.as_mut().unwrap().right = right_then_left_child.take();
+    fix_height(old_root_x);
+    old_right_child.as_mut().unwrap().left = old_root_x.take();
+    old_right_child.as_mut().unwrap().height = new_height;
+    *old_root_x = old_right_child.take();
+    fix_height(old_root_x);
     //    *tree = new_root_z;
     // at first tree -> X and X.right -> Z and Z.left -> t23
     // swap tree and Z.left
     // now tree -> t23 and X.right -> Z and Z.left -> X
     // swap tree and X.right
     // now tree -> Z and X.right -> t23 and Z.left -> X
+}
+
+fn rotate_right<T: Ord>(old_root_x: &mut AVLTree<T>) {
+    if old_root_x.as_mut().is_none() {
+        return;
+    }
+    if old_root_x.as_mut().unwrap().left.as_mut().is_none() {
+        panic!("I am rotating right but my left child is None.");
+    }
+    let left_then_right_child: &mut AVLTree<T> = &mut old_root_x
+        .as_mut()
+        .unwrap()
+        .left
+        .as_mut()
+        .unwrap()
+        .right
+        .take();
+    let old_left_child: &mut AVLTree<T> = &mut old_root_x.as_mut().unwrap().left.take();
+    let left_height = get_height(left_then_right_child);
+    let right_height = get_height(&old_root_x.as_mut().unwrap().right);
+    println!(
+        "New right height is {0} and left height is {1}",
+        right_height, left_height
+    );
+    let new_height: i64 = 1 + max(right_height, left_height);
+    old_root_x.as_mut().unwrap().left = left_then_right_child.take();
+    fix_height(old_root_x);
+    old_left_child.as_mut().unwrap().right = old_root_x.take();
+    old_left_child.as_mut().unwrap().height = new_height;
+    *old_root_x = old_left_child.take();
+    fix_height(old_root_x);
+    //    *tree = new_root_z;
+    // at first tree -> X and X.left -> Z and Z.right -> t23
+    // swap tree and Z.right
+    // now tree -> t23 and X.left -> Z and Z.right -> X
+    // swap tree and X.left
+    // now tree -> Z and X.left -> t23 and Z.right -> X
+}
+
+fn rotate_left_maybe_double<T: Ord + Debug>(tree: &mut AVLTree<T>) {
+    if tree.as_ref().is_none() {
+        return;
+    }
+    let node = tree.as_mut().unwrap();
+    println!("I am going to rotate left from {0:?}", node.label);
+    if balance_factor(&node.right) > 0 {
+        println!(
+            "But first I must rotate right from {0:?}",
+            node.right.as_ref().unwrap().label
+        );
+        rotate_right(&mut node.right);
+        fix_height(tree);
+    }
+    rotate_left(tree);
+}
+
+fn rotate_right_maybe_double<T: Ord + Debug>(tree: &mut AVLTree<T>) {
+    if tree.as_ref().is_none() {
+        return;
+    }
+    let node = tree.as_mut().unwrap();
+    println!("I am going to rotate right from {0:?}", node.label);
+    if balance_factor(&node.left) < 0 {
+        println!(
+            "But first I must rotate left from {0:?}",
+            node.left.as_ref().unwrap().label
+        );
+        rotate_left(&mut node.left);
+        fix_height(tree);
+    }
+    rotate_right(tree);
+}
+
+fn in_order<T: Ord + Copy>(tree: &AVLTree<T>) -> Vec<T> {
+    if tree.as_ref().is_none() {
+        let empty: Vec<T> = vec![];
+        return empty;
+    }
+    let node = tree.as_ref().unwrap();
+    let mut from_left = in_order(&node.left);
+    let from_right = in_order(&node.right);
+    from_left.push(node.label);
+    from_left.extend(from_right);
+    from_left
+}
+
+fn fix_height<T: Ord>(tree: &mut AVLTree<T>) {
+    // This only works if your two subtrees have accurate height. So fix them
+    // first.
+    let option_amp_mut: Option<&mut Box<AVLNode<T>>> = tree.as_mut();
+    if option_amp_mut.is_some() {
+        let mut_box: &mut Box<AVLNode<T>> = option_amp_mut.unwrap();
+        let left_height = get_height(&mut_box.left);
+        let right_height = get_height(&mut_box.right);
+        println!(
+            "Before fixing the height, left height is {0} and right height is {1}",
+            left_height, right_height
+        );
+        let new_height = 1 + max(left_height, right_height);
+        mut_box.height = new_height;
+    }
 }
 
 fn main() {
@@ -158,4 +243,18 @@ fn main() {
     println!("The tree is now of height {0},", get_height(&my_tree));
     println!("Does the tree contain c? {0}", contains(&my_tree, 'c'));
     println!("Does the tree contain z? {0}", contains(&my_tree, 'z'));
+    println!("All elements in order: {0:?}", in_order(&my_tree));
+
+    println!(
+        "Before rotating the label is {0} and the height is {1}",
+        my_tree.as_ref().unwrap().label,
+        get_height(&my_tree)
+    );
+    rotate_left(&mut my_tree);
+    println!(
+        "After rotating the label is {0} and the height is {1}",
+        my_tree.as_ref().unwrap().label,
+        get_height(&my_tree)
+    );
+    println!("All elements in order: {0:?}", in_order(&my_tree));
 }
