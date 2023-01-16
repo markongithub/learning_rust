9a59683852737b6b359d21575f6e10b4b3b29fd1
mark on github <markongithub@users.noreply.github.com>
2023-01-16 09:16:47 -0500

    seems to work, commit before cleaning up

--- aoc_2022_day09.rs
+++ aoc_2022_day09.rs
@@ -15,9 +15,10 @@ struct Coords {
 
 #[derive(Debug)]
 struct RopeState {
-    head: Coords,
-    tail: Coords,
+    knots: Vec<Coords>,
     visited: HashSet<Coords>,
+    time: usize,
+    max_knots: usize,
 }
 
 fn parse_motion(line: &str) -> Motion {
@@ -37,7 +38,7 @@ fn parse_input(one_big_string: &str) -> Vec<Motion> {
     motions
 }
 
-fn move_head(old: Coords, direction: char) -> Coords {
+fn move_head(old: &Coords, direction: char) -> Coords {
     match direction {
         'U' => Coords {
             x: old.x,
@@ -60,44 +61,38 @@ fn move_head(old: Coords, direction: char) -> Coords {
 }
 
 fn move_one_step(old: RopeState, direction: char) -> RopeState {
-    let new_head = move_head(old.head, direction);
+    let mut new_knots: Vec<Coords> = vec![];
 
-    let new_tail = if new_head.x.abs_diff(old.tail.x) <= 1 && new_head.y.abs_diff(old.tail.y) <= 1 {
-        old.tail
-    } else if new_head.x == old.tail.x + 2 {
-        Coords {
-            x: old.tail.x + 1,
-            y: new_head.y,
-        }
-    } else if new_head.x == old.tail.x - 2 {
-        Coords {
-            x: old.tail.x - 1,
-            y: new_head.y,
-        }
-    } else if new_head.y == old.tail.y + 2 {
-        Coords {
-            x: new_head.x,
-            y: old.tail.y + 1,
-        }
-    } else if new_head.y == old.tail.y - 2 {
-        Coords {
-            x: new_head.x,
-            y: old.tail.y - 1,
-        }
-    } else {
-        panic!("I fucked up the head/tail cases.")
+    let mut knots_iter = old.knots.iter();
+    let old_head: &Coords = knots_iter.next().unwrap();
+    let mut new_head = move_head(old_head, direction);
+    let mut new_head_copy = Coords {
+        x: new_head.x,
+        y: new_head.y,
     };
+    new_knots.push(new_head_copy);
+    for old_knot in knots_iter {
+        new_head = update_tail_from_head(&new_head, old_knot);
+        // println!("... so I moved to {:?}", new_head);
+        new_head_copy = Coords {
+            x: new_head.x,
+            y: new_head.y,
+        };
+        new_knots.push(new_head_copy);
+    }
+    // we still haven't updated visited
+    // we only do that when we place the last knot
+    // I think maybe instead of an iterator we want to use indices?
+    // no wait, we still have one new_head left over from up above.
     let mut visited = old.visited; // I want to take the value here
-    visited.insert(Coords {
-        x: new_tail.x,
-        y: new_tail.y,
-    });
+    visited.insert(new_head);
     let output = RopeState {
-        head: new_head,
-        tail: new_tail,
+        knots: new_knots,
         visited: visited,
+        time: old.time + 1,
+        max_knots: old.max_knots,
     };
-    //    println!("{:?}", output);
+    // println!("{:?}", output);
     output
 }
 
@@ -106,10 +101,12 @@ fn run_motion(old: RopeState, motion: &Motion) -> RopeState {
         "We are at {},{} and will now go {} by {} steps.",
         old.head.x, old.head.y, motion.direction, motion.steps
     );*/
+    // wait, why am I copying this?
     let mut output = RopeState {
-        head: old.head,
-        tail: old.tail,
+        knots: old.knots,
         visited: old.visited,
+        time: old.time,
+        max_knots: old.max_knots,
     };
     for _i in 1..=motion.steps {
         output = move_one_step(output, motion.direction);
@@ -117,27 +114,21 @@ fn run_motion(old: RopeState, motion: &Motion) -> RopeState {
     output
 }
 
-fn run_motions(motions: Vec<Motion>) -> RopeState {
-    let mut motions_iter = motions.iter();
-    let first_motion = motions_iter.next().unwrap();
-    let initial_head = move_head(Coords { x: 0, y: 0 }, first_motion.direction);
-    let initial_tail = Coords { x: 0, y: 0 };
-    let mut initial_visited: HashSet<Coords> = HashSet::new();
-    initial_visited.insert(Coords { x: 0, y: 0 });
+fn run_motions(motions: Vec<Motion>, max_knots: usize) -> RopeState {
+    let mut initial_knots = vec![];
+    for _i in 1..=max_knots {
+        initial_knots.push(Coords { x: 0, y: 0 });
+    }
 
-    let rest_of_first_motion = Motion {
-        direction: first_motion.direction,
-        steps: first_motion.steps - 1,
+    let mut initial_visited = HashSet::new();
+    initial_visited.insert(Coords { x: 0, y: 0 });
+    let mut next_state = RopeState {
+        knots: initial_knots,
+        visited: initial_visited,
+        time: 1,
+        max_knots: max_knots,
     };
-    let mut next_state = run_motion(
-        RopeState {
-            head: initial_head,
-            tail: initial_tail,
-            visited: initial_visited,
-        },
-        &rest_of_first_motion,
-    );
-    for motion in motions_iter {
+    for motion in motions.iter() {
         next_state = run_motion(next_state, motion);
     }
     next_state
@@ -145,10 +136,81 @@ fn run_motions(motions: Vec<Motion>) -> RopeState {
 
 fn solve_part_1(input: &str) -> usize {
     let motions = parse_input(input);
-    let final_state = run_motions(motions);
+    let final_state = run_motions(motions, 2);
     final_state.visited.len()
 }
 
+fn solve_part_2(input: &str) -> usize {
+    let motions = parse_input(input);
+    let final_state = run_motions(motions, 10);
+    final_state.visited.len()
+}
+
+fn update_tail_from_head(new_head: &Coords, old_tail: &Coords) -> Coords {
+    //println!(
+    //    "I am at {:?} and my leader is at {:?}...",
+    //    old_tail, new_head
+    //);
+    let new_tail = if new_head.x.abs_diff(old_tail.x) <= 1 && new_head.y.abs_diff(old_tail.y) <= 1 {
+        Coords {
+            x: old_tail.x,
+            y: old_tail.y,
+        }
+    } else if (new_head.x == old_tail.x + 2) && (new_head.y == old_tail.y + 2) {
+        Coords {
+            x: old_tail.x + 1,
+            y: old_tail.y + 1,
+        }
+    } else if new_head.x == old_tail.x + 2 && new_head.y == old_tail.y - 2 {
+        Coords {
+            x: old_tail.x + 1,
+            y: old_tail.y - 1,
+        }
+    } else if new_head.x == old_tail.x - 2 && new_head.y == old_tail.y + 2 {
+        Coords {
+            x: old_tail.x - 1,
+            y: old_tail.y + 1,
+        }
+    } else if new_head.x == old_tail.x - 2 && new_head.y == old_tail.y - 2 {
+        Coords {
+            x: old_tail.x - 1,
+            y: old_tail.y - 1,
+        }
+    } else if new_head.x == old_tail.x + 2 {
+        Coords {
+            x: old_tail.x + 1,
+            y: new_head.y,
+        }
+    } else if new_head.x == old_tail.x - 2 {
+        Coords {
+            x: old_tail.x - 1,
+            y: new_head.y,
+        }
+    } else if new_head.y == old_tail.y + 2 {
+        Coords {
+            x: new_head.x,
+            y: old_tail.y + 1,
+        }
+    } else if new_head.y == old_tail.y - 2 {
+        Coords {
+            x: new_head.x,
+            y: old_tail.y - 1,
+        }
+    } else {
+        panic!(
+            "I fucked up the head/tail cases. new_head: {:?}, old_tail: {:?}",
+            new_head, old_tail
+        )
+    };
+    if manhattan_distance(&new_tail, old_tail) > 2 {
+        panic!("Why did we move from {:?} to {:?}?", old_tail, new_tail)
+    }
+    new_tail
+}
+
+fn manhattan_distance(a: &Coords, b: &Coords) -> u64 {
+    a.x.abs_diff(b.x) + a.y.abs_diff(b.y)
+}
 fn main() {
     let test_input = "R 4
 U 4
@@ -158,7 +220,18 @@ R 4
 D 1
 L 5
 R 2";
+    let test_input_2 = "R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20";
     println!("Part 1 test: {}", solve_part_1(test_input));
     let real_input = read_to_string("data/input09.txt").unwrap();
     println!("Part 1 solution: {:?}", solve_part_1(&real_input));
+    println!("Part 2 test 1: {}", solve_part_2(test_input));
+    println!("Part 2 test 2: {}", solve_part_2(test_input_2));
+    println!("Part 2 solution: {:?}", solve_part_2(&real_input));
 }
